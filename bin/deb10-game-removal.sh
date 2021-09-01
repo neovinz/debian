5e3c911ab951c1bfa068acc77d88054d758421f9
vesteves <vesteves@wooxo.fr>
2021-09-01 17:55:05 +0200

    added remove game aisleriot

--- bin/deb10-game-removal.sh
+++ bin/deb10-game-removal.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# remove all games
+sudo apt remove five-or-more four-in-a-row iagno gnome-mines gnome-sudoku gnome-taquin gnome-tetravex gnome-robots\
+ gnome-mahjongg quadrapassel hoichess gnome-klotski gnome-nibbles swell-foop hitori tali lightsoff aisleriot
 
-sudo apt remove five-or-more four-in-a-row iagno gnome-mines gnome-sudoku gnome-taquin gnome-tetravex gnome-robots gnome-mahjongg quadrapassel hoichess gnome-klotski gnome-nibbles swell-foop hitori tali lightsoff
+# clean
+sudo apt autoremove
