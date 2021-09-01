5f2a2af81228ca1d705da5e79c64cc18246eb39e
vesteves@wooxo.fr <vesteves@wooxo.fr>
2021-09-01 17:49:15 +0200

    deb10 games removal

---
+++ bin/deb10-game-removal.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+sudo apt remove five-or-more four-in-a-row iagno gnome-mines gnome-sudoku gnome-taquin gnome-tetravex gnome-robots gnome-mahjongg quadrapassel hoichess gnome-klotski gnome-nibbles swell-foop hitori tali lightsoff
