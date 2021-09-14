4c1918d2602e1ca7a6cbc0e20c4c8db2de327128
Vincent ESTEVES <vinz.neo@gmail.com>
2021-09-14 22:24:29 +0200

    added history alias

--- bin/deb10-sublime-text.sh
+++ bin/deb10-sublime-text.sh
@@ -17,6 +17,11 @@ sudo apt update
 # Install Sublime Text 3
 sudo apt install sublime-text sublime-merge
 
+# In some scenarios Sublime Text fails to see files mounted via SFTP protocol with
+# graphical file managers like Nautilus, Thunar, or Dolphin, and although files are visible
+# throughout the system, they are inaccessible in Open file dialog. To fix the issue and
+# enable remote file editing functionality in Sublime Text
+sudo apt-get install gvfs-backends gvfs-fuse
 
 # Display message to uninstall it
 echo "To uninstall run : \n sudo apt-get remove sublime-text sublime-merge && sudo apt-get autoremove"
--- terminal/deb9_wsl.bash_aliases
+++ terminal/deb9_wsl.bash_aliases
@@ -1,8 +1,9 @@
 alias myip="/sbin/ifconfig | grep -Eo 'inet (adr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'"
+alias h='history'
 # alias cat='cat -n'
 alias hl='sh /home/$USER/bin/usefull-commands.sh'
 alias phpv='sudo update-alternatives --config php'
 alias phps='php -S localhost:8888'
 alias lsb='lsb_release -a'
 alias ws='sudo du -h --max-depth=1 ./ | sudo sort -hr'
-alias ptr='docker run -rm -d -p 8000:8000 -p 9900:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce'
+alias ptr='docker start portainer'
