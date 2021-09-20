f1ff73e64f165899a149b1cce22250480294f358
Vincent ESTEVES <vinz.neo@gmail.com>
2021-09-21 00:56:44 +0200

    added open-ssh

--- bin/basics.sh
+++ bin/basics.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt install nano lsb-base curl bash-completion htop net-tools zip unzip wget lsb-release screen dos2unix
+sudo apt install nano lsb-base curl bash-completion htop net-tools zip unzip wget lsb-release screen dos2unix openssh-server
 
