2e4b978d54c56dbf9210ab4f70bcea3830d80fe2
Vincent <vesteves@wooxo.fr>
2021-11-26 15:02:54 +0100

    easier name for bash_aliases file

--- bin/sync-aliases.sh
+++ bin/sync-aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm .bash_aliases
-cp ~/terminal/deb9_wsl.bash_aliases ~/.bash_aliases_git
+cp ~/terminal/.bash_aliases ~/.bash_aliases_git
 cat ~/.bash_aliases* > ~/.bash_aliases
 
 . ~/.bash_aliases
