114689d0af09e48a2f1c6989f82f729c547a508a
Vincent ESTEVES <vinz.neo@gmail.com>
2023-06-07 23:36:38 +0200

    remove -y option

--- bin/basics.sh
+++ bin/basics.sh
@@ -7,10 +7,11 @@ sudo apt update
 sudo apt update
 
 # remove programs
-sudo apt remove rhythmbox evolution thunderbird lightsoff polari
+sudo apt remove rhythmbox  thunderbird lightsoff polari
+sudo apt remove evolution
 
 # remove libreoffice
-#sudo apt romove libreoffice-writer libreoffice-calc libreoffice-base libreoffice-draw libreoffice-impress libreoffice
+sudo apt remove libreoffice-writer libreoffice-calc libreoffice-base libreoffice-draw libreoffice-impress libreoffice
 
 # remove games
 sudo apt remove five-or-more four-in-a-row iagno gnome-mines gnome-sudoku gnome-taquin gnome-tetravex gnome-robots gnome-mahjongg quadrapassel hoichess gnome-klotski gnome-nibbles hdate-applet swell-foop hitori tali lightsoff
@@ -20,10 +21,10 @@ sudo apt upgrade
 sudo apt dist-upgrade
 
 # install programs
-sudo apt install -y nano tree lsb-base curl bash-completion htop net-tools zip unzip wget lsb-release screen dos2unix openssh-server
-sudo apt install -y firmware-misc-nonfree
-sudo apt -y install gimp chromium filezilla git apt-transport-https
-sudo apt -y install hddtemp lm-sensors
+sudo apt install nano tree lsb-base curl bash-completion htop net-tools zip unzip wget lsb-release screen dos2unix openssh-server
+sudo apt install firmware-misc-nonfree
+sudo apt install gimp chromium filezilla git apt-transport-https
+sudo apt install hddtemp lm-sensors
 
 #printers
 sudo apt install task-print-server
