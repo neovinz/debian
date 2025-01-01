78c7f157070502b62d4fb78637004cb9c80c36bc
Vincent ESTEVES <vinz.neo@gmail.com>
2025-01-02 00:42:44 +0100

    remove unused languages

---
+++ bin/remove-aspell.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+
+sudo apt remove aspell-bg aspell-ca aspell-cs aspell-cy aspell-da aspell-de aspell-el aspell-eo aspell-es aspell-et aspell-eu aspell-fa aspell-ga aspell-gl-minimos aspell-is aspell-it aspell-kk aspell-ku aspell-lt aspell-lv aspell-nl aspell-no aspell-sk aspell-sl aspell-sv aspell-tl
+
+sudo apt remove myspell-eo myspell-es myspell-et myspell-ga myspell-he myspell-nb myspell-nn myspell-sk myspell-sq
+
+
+sudo apt remove hunspell-ar hunspell-be hunspell-bg hunspell-bs hunspell-ca hunspell-cs hunspell-da hunspell-de-at hunspell-de-ch hunspell-de-de hunspell-gl hunspell-gu hunspell-hi hunspell-hr hunspell-hu hunspell-id hunspell-is hunspell-it hunspell-kk hunspell-kmr hunspell-ko hunspell-lt hunspell-lv hunspell-ne hunspell-nl hunspell-pl hunspell-pt-br hunspell-pt-pt hunspell-ro hunspell-ru hunspell-se hunspell-si hunspell-sl hunspell-sr hunspell-sv hunspell-te hunspell-th hunspell-vi
+
+
+sudo apt autoremove
+sudo apt autoclean
